2feb3bc3b0ffc7d3a3a95274cecb03df346379fe
yoon-chaejin <kevin3st@naver.com>
2023-10-25 23:08:19 +0900

    initialize project

---
+++ src/test/java/com/songchuwe/bingo/BingoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.songchuwe.bingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
